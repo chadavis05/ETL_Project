5648e8c2084a241ed3e9d22a124d26f524c115fa
Chad Davis <ccdavis2k@gmail.com>
2020-05-28 19:50:49 -0400

    project complete

---
+++ schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE states_gdp (
+  state TEXT primary key,
+  gdp INT
+);
+CREATE TABLE states_pop (
+  state TEXT primary key,
+  den_per_sqm INT,
+  pop_numbers INT,
+  land_sqm INT
+);
